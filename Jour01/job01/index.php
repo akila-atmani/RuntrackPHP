09df05065b657603c4b3203c35694a0c83449cf2
akila-atmani <akila.atmani@laplateforme.io>
2025-01-13 12:01:35 +0100

    Job01

---
+++ Jour01/job01/index.php
@@ -0,0 +1,48 @@
+<?php
+   
+    $str = "LaPlateforme";
+    
+    echo $str;
+?>
+
+
+
+
+
+<?php
+ 
+    $str2 = "Vive";
+    $str = "LaPlateforme";
+    $str3 = "!";
+
+ 
+    echo $str2 . " " . $str . " " . $str3;
+?>
+
+
+<?php
+   
+    $val = 6;
+    
+ 
+    echo "Valeur initiale : " . $val . "<br>";
+
+   
+    $val += 4;
+
+    echo "Valeur après addition : " . $val;
+?>
+
+<?php
+
+    $myBool = true;
+
+    echo "Valeur initiale : " . ($myBool ? "true" : "false") . "<br>";
+
+
+    $myBool = false;
+
+
+    echo "Valeur après modification : " . ($myBool ? "true" : "false");
+?>
+
