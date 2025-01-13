bb8a9f4c20cdff6802afaad916bae0f04a4651e8
akila-atmani <akila.atmani@laplateforme.io>
2025-01-13 13:24:24 +0100

    job03

---
+++ Jour01/job03/index.php
@@ -0,0 +1,17 @@
+<?php
+
+    $isBoolean = true;
+
+
+    $integer = 2;
+
+    $string = "Coucou";
+
+    $float = 1.4;
+
+
+    echo "Boolean : " . ($isBoolean ? "true" : "false") . "<br>";
+    echo "Entier : " . $integer . "<br>" ;
+    echo "Chaîne de caractères : " . $string . "<br>";
+    echo "Nombre flottant : " . $float . "<br>";
+?>
