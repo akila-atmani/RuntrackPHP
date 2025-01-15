5ed3d88c37e0085be34e2fa7aaeca071106da4a1
akila-atmani <akila.atmani@laplateforme.io>
2025-01-15 10:43:19 +0100

    job03

---
+++ Jour03/job03/index.php
@@ -0,0 +1,17 @@
+<?php
+
+$str = "I'm sorry Dave I'm afraid I can't do that";
+
+$voyelles = "aeiouyAEIOUY";
+
+
+for ($i = 0; isset($str[$i]); $i++) {
+    
+    for ($j = 0; isset($voyelles[$j]); $j++) {
+        if ($str[$i] === $voyelles[$j]) {
+            echo $str[$i]; 
+       
+        }
+    }
+}
+?>
