d1d212be4a8b26bc48d8607eaa5edba31d712bed
akila-atmani <akila.atmani@laplateforme.io>
2025-01-17 13:09:21 +0100

    job06

---
+++ Jour05/job06/index.php
@@ -0,0 +1,36 @@
+<?php
+
+function convertToLeet($str) {
+   
+    $leetStr = "";
+
+    for ($i = 0; $i < strlen($str); $i++) {
+        $char = $str[$i];
+
+        if ($char === 'A' || $char === 'a') {
+            $leetStr .= '4';
+        } elseif ($char === 'E' || $char === 'e') {
+            $leetStr .= '3';
+        } elseif ($char === 'I' || $char === 'i') {
+            $leetStr .= '1';
+        } elseif ($char === 'O' || $char === 'o') {
+            $leetStr .= '0';
+        } elseif ($char === 'S' || $char === 's') {
+            $leetStr .= '5';
+        } elseif ($char === 'T' || $char === 't') {
+            $leetStr .= '7';
+
+
+        } else {
+           $leetStr .= $char;
+        }
+    }
+
+ 
+    return $leetStr;
+}
+
+
+$chaine = "Coucou , comment ça va ?";
+echo "En leet speak : " . convertToLeet($chaine);
+?>
